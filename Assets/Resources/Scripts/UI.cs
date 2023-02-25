96bd09bd9dcf746e23d03a16db6bbeded91168c7
Yeghiazaryan Smbat <smbat_eghiazaryan20@alumni.aua.am>
2023-02-26 00:09:07 +0400

    added CurvedWorld to the project

--- Assets/Resources/Scripts/UI.cs
+++ Assets/Resources/Scripts/UI.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public void showMenu(bool isWin)
-    {
-        if (isWin)
-            ;//change text to u Win
-        //show text
-        //show score
-        //show menu
-    }
+
 }
